c01591109c9ef2eae1fc599f230c2299db4f7a86
vaishnavi krishna <krishnavaishnavi2005@gmail.com>
2025-08-12 02:58:10 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class main {//ANIMAL class
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+Laptop laptop = new Laptop("ios16","iphone",2056,256,14,1800);
+laptop.display();
+   }
+}
